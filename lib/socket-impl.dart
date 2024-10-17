bf94e48a3c462183296ddd0e97c621197c22e571
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-17 22:20:17 +0530

    frontend socketss

--- lib/socket-impl.dart
+++ lib/socket-impl.dart
@@ -1,9 +1,21 @@
 import 'dart:io';
 
-void main() {
-  var sock = Socket.connect('0.0.0.0', 12345);
+void main() async {
+  // Connect to the server
+  final socket = await Socket.connect('localhost', 12345);
+  print('Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
 
+  // Send data to the server
+  socket.writeln('Hello, Server!');
+
+  // Listen for responses from the server
+  socket.listen(
+    (data) {
+      print('Server: ${String.fromCharCodes(data).trim()}');
+    },
+    onDone: () {
+      print('Server closed the connection');
+      socket.destroy();
+    },
+  );
 }
-
-
-
